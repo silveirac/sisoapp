dc6e5490d127628def2b376754fc3b988a914490
Paulo Borges <paulinhodevelover@outlook.com>
2023-04-03 22:13:51 -0300

    Ajustado projeto para retornar na consulta o status no BODY

--- Backend/src/main/java/com/dh/sisoapp/service/ConsultaService.java
+++ Backend/src/main/java/com/dh/sisoapp/service/ConsultaService.java
@@ -6,7 +6,10 @@ import com.dh.sisoapp.repository.ConsultaRepository;
 import com.dh.sisoapp.repository.DentistaRepository;
 import com.dh.sisoapp.repository.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
@@ -26,55 +29,80 @@ public class ConsultaService {
         this.pacienteRepository = pacienteRepository;
         this.dentistaRepository = dentistaRepository;
     }
-
-    public void salvar(Consulta consulta) {
+    public ResponseEntity<String> salvar(Consulta consulta) {
         // Verifica se paciente e dentista existem no sistema
         if (!pacienteRepository.existsById(consulta.getPaciente().getId())) {
             Util.escreveLog("Paciente não encontrado");
-            throw new IllegalArgumentException("Paciente não encontrado");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Paciente não encontrado");
         }
         if (!dentistaRepository.existsById(consulta.getDentista().getId())) {
             Util.escreveLog("Dentista não encontrado");
-            throw new IllegalArgumentException("Dentista não encontrado");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Dentista não encontrado");
         }
         Util.escreveLog("Salvando consulta: "+ consulta);
         consultaRepository.save(consulta);
         Util.escreveLog("Consulta salva com sucesso: "+consulta);
+        return ResponseEntity.status(HttpStatus.CREATED).body(String.valueOf(consulta));
     }
-
-    public List<Consulta> listar() {
-        return consultaRepository.findAll();
+    public ResponseEntity<?> listar() {
+        List<Consulta> consultas = consultaRepository.findAll();
+        if (consultas.isEmpty()) {
+            Util.escreveLog("Não existem consultas cadastradas");
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Não existem consultas cadastradas");
+        } else {
+            return ResponseEntity.ok(consultas);
+        }
     }
-
-    public Consulta consultarPorId(Long id) {
-        return consultaRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Consulta não encontrada"));
-    }
-
-    public void excluir(Long id) {
-        if (!consultaRepository.existsById(id)) {
+    public ResponseEntity<?> consultarPorId(Long id) {
+        try {
+            Consulta consulta = consultaRepository.findById(id)
+                    .orElseThrow(() -> new IllegalArgumentException("Consulta não encontrada"));
+            return ResponseEntity.ok(consulta);
+        } catch (IllegalArgumentException e) {
             Util.escreveLog("Consulta não encontrada");
-            throw new IllegalArgumentException("Consulta não encontrada");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Consulta não encontrada");
         }
-        consultaRepository.deleteById(id);
+    }
+    public ResponseEntity<?> excluir(Long id) {
+        try {
+            if (!consultaRepository.existsById(id)) {
+                Util.escreveLog("Consulta não encontrada");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Consulta não encontrada");
+            }
+            consultaRepository.deleteById(id);
+            Util.escreveLog("Consulta excluída com sucesso");
+            return ResponseEntity.ok("Consulta excluída com sucesso");
+        } catch (Exception e) {
+            Util.escreveLog("Erro ao excluir consulta: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao excluir consulta");
+        }
     }
 
-    public void atualizar(Consulta consulta) {
-        // Verifica se a consulta existe no sistema
-        if (!consultaRepository.existsById(consulta.getId())) {
-            Util.escreveLog("Consulta não encontrada");
-            throw new IllegalArgumentException("Consulta não encontrada");
-        }
+    public ResponseEntity<?> atualizar(Consulta consulta) {
+        try {
+            // Verifica se a consulta existe no sistema
+            if (!consultaRepository.existsById(consulta.getId())) {
+                Util.escreveLog("Consulta não encontrada");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Consulta não encontrada");
+            }
 
-        // Verifica se paciente e dentista existem no sistema
-        if (!pacienteRepository.existsById(consulta.getPaciente().getId())) {
-            Util.escreveLog("Paciente não encontrado");
-            throw new IllegalArgumentException("Paciente não encontrado");
+            // Verifica se paciente e dentista existem no sistema
+            if (!pacienteRepository.existsById(consulta.getPaciente().getId())) {
+                Util.escreveLog("Paciente não encontrado");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Paciente não encontrado");
+            }
+            if (!dentistaRepository.existsById(consulta.getDentista().getId())) {
+                Util.escreveLog("Dentista não encontrado");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Dentista não encontrado");
+            }
+
+            consultaRepository.save(consulta);
+            Util.escreveLog("Consulta atualizada com sucesso");
+            return ResponseEntity.ok("Consulta atualizada com sucesso");
+        } catch (Exception e) {
+            Util.escreveLog("Erro ao atualizar consulta: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao atualizar consulta");
         }
-        if (!dentistaRepository.existsById(consulta.getDentista().getId())) {
-            Util.escreveLog("Dentista não encontrada");
-            throw new IllegalArgumentException("Dentista não encontrado");
-        }
-        consultaRepository.save(consulta);
     }
+
 }
