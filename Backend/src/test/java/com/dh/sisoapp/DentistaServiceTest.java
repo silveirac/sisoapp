fdc4234158695c06c1ecec8fa06a49a1dbec8652
Paulo Borges <paulinhodevelover@outlook.com>
2023-04-04 19:20:44 -0300

    ajustado projeto para funcionar com teste automatizados

--- Backend/src/test/java/com/dh/sisoapp/DentistaServiceTest.java
+++ Backend/src/test/java/com/dh/sisoapp/DentistaServiceTest.java
@@ -8,13 +8,13 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 public class DentistaServiceTest {
