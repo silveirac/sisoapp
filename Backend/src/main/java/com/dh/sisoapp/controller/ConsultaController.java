037d1fa0318eed17ec85971d553078d22e5782bd
Paulo Borges <paulinhodevelover@outlook.com>
2023-04-05 20:15:56 -0300

    Ajustado Controlador de ConsultaController

--- Backend/src/main/java/com/dh/sisoapp/controller/ConsultaController.java
+++ Backend/src/main/java/com/dh/sisoapp/controller/ConsultaController.java
@@ -5,55 +5,62 @@ import com.dh.sisoapp.service.ConsultaService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
 @RestController
 @RequestMapping("/consultas")
 public class ConsultaController {
     private final ConsultaService consultaService;
+
     public ConsultaController(ConsultaService consultaService) {
         this.consultaService = consultaService;
     }
+
     @PostMapping
-    public ResponseEntity<Void> cadastrarConsulta(@RequestBody Consulta consulta) {
+    public ResponseEntity<Object> cadastrarConsulta(@RequestBody Consulta consulta) {
         try {
             consultaService.salvar(consulta);
-            return ResponseEntity.ok().build();
+            return new ResponseEntity<>("Sucess", HttpStatus.CREATED);
         } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().build();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 
     @GetMapping
-    public List<Consulta> listarConsultas() {
-        return consultaService.listar();
+    public ResponseEntity<Object> listarConsultas() {
+        try {
+            return new ResponseEntity<>(consultaService.listar(), HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Object> consultarPorId(@PathVariable Long id) {
         try {
             Consulta consulta = consultaService.consultarPorId(id);
-            return ResponseEntity.ok(consulta);
+            return new ResponseEntity<>(consulta, HttpStatus.OK);
         } catch (IllegalArgumentException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> excluirConsulta(@PathVariable Long id) {
+    public ResponseEntity<Object> excluirConsulta(@PathVariable Long id) {
         try {
             consultaService.excluir(id);
-            return ResponseEntity.ok().build();
+            return new ResponseEntity<>("Sucess", HttpStatus.NO_CONTENT);
         } catch (IllegalArgumentException e) {
-            return ResponseEntity.notFound().build();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
+
     @PutMapping()
-    public ResponseEntity<Void> atualizarConsulta(@RequestBody Consulta consulta) {
+    public ResponseEntity<Object> atualizarConsulta(@RequestBody Consulta consulta) {
         try {
             consultaService.atualizar(consulta);
-            return ResponseEntity.ok().build();
+            return new ResponseEntity<>(consulta, HttpStatus.OK);
         } catch (IllegalArgumentException e) {
-            return ResponseEntity.notFound().build();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
 }
